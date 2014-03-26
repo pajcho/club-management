c9a53147ca33c39207ccd9dbce1f392de52189dc
Nikola Pajic <nikola.pajic@gmail.com>
2014-03-26 20:27:01 +0100

    Add custom cookie name for environments

---
+++ app/config/stage/session.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session Cookie Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may change the name of the cookie used to identify a session
+	| instance by ID. The name specified here will get used every time a
+	| new session cookie is created by the framework for every driver.
+	|
+	*/
+
+	'cookie' => 'club_management_stage_session',
+
+);
