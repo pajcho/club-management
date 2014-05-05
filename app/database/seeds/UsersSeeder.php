9dd2a5f03914279e19191f9449be9586ce2a9540
Nikola Pajic <nikola.pajic@gmail.com>
2014-05-05 20:50:16 +0200

    Update user seeder and repository interface

--- app/database/seeds/UsersSeeder.php
+++ app/database/seeds/UsersSeeder.php
@@ -13,9 +13,12 @@ class UsersSeeder extends Seeder {
 
         // Add your users in this form
 //        array_push($users, array(
+//            'first_name' => 'first_name',
+//            'last_name' => 'last_name',
 //            'username' => 'username',
 //            'email' => 'email',
 //            'password' => Hash::make('password'),
+//            'type' => 'admin', // can be 'admin' or 'trainer'
 //        ));
 
 		// Delete all users
