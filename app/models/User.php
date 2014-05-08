a5ba9a6e9a12824887cb8a0ab2d8a8afbb4916b0
Nikola Pajic <nikola.pajic@gmail.com>
2014-05-08 22:03:01 +0200

    Cleanup
    - Returned new root user model to initial version because it is not
    used anymore

--- app/models/User.php
+++ app/models/User.php
@@ -1,133 +1,83 @@
 <?php
 
-use App\Internal\HistorableTrait;
-use App\Models\BaseModel;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
-use Illuminate\Support\Facades\Hash;
 
-class User extends BaseModel implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface {
 
-    /** History */
-    use HistorableTrait;
-    public function historyTable()
-    {
-        return 'user';
-    }
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-    public function historyTitle()
-    {
-        return $this->full_name;
-    }
-
-    public $timestamps = true;
-
-    protected $table = 'users123';
-    protected $softDelete = false;
-
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
     protected $hidden = array('password');
-    protected $fillable = array('first_name', 'last_name', 'username', 'email', 'phone', 'address', 'notes', 'password', 'type');
-    protected $appends = array('full_name', 'group_ids');
-
-    public function groups()
-    {
-        return $this->belongsToMany('App\Models\MemberGroup', 'users_groups', 'user_id', 'group_id')->withTimestamps();
-    }
 
     /**
-     * Hash password automaticaly when creating user
+     * Get the unique identifier for the user.
      *
-     * @param $value
+     * @return mixed
      */
-    public function setPasswordAttribute($value)
+    public function getAuthIdentifier()
     {
-        $this->attributes['password'] = Hash::make($value);
+        return $this->getKey();
     }
 
     /**
-     * Check to see if user is admin
-     *
-     * @return bool
-     */
-    public function isAdmin()
-    {
-        return $this->type == 'admin';
-    }
-
-    /**
-     * Check to see if user is trainer
-     *
-     * @return bool
-     */
-    public function isTrainer()
-    {
-        return $this->type == 'trainer';
-    }
-
-    /**
-     * Get user full name
+     * Get the password for the user.
      *
      * @return string
      */
-    public function getFullNameAttribute()
+    public function getAuthPassword()
     {
-        return $this->attributes['first_name'] . ' ' . $this->attributes['last_name'];
+        return $this->password;
     }
 
     /**
-     * Get ids of all groups user is in
-     * Used mainly for select boxes
+     * Get the token value for the "remember me" session.
      *
      * @return string
      */
-    public function getGroupIdsAttribute()
-    {
-        return $this->groups()->get()->lists('id');
-    }
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
-
     public function getRememberToken()
     {
         return $this->remember_token;
     }
 
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
     public function setRememberToken($value)
     {
         $this->remember_token = $value;
     }
 
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
     public function getRememberTokenName()
     {
         return 'remember_token';
+    }
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
     }
 
 }
