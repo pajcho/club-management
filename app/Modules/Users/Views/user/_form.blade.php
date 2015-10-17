9872c20b6a7de5b7c45d0c023720a0202573b788
Nikola Pajic <nikola.pajic@gmail.com>
2015-10-17 23:49:15 +0200

    Improve user details page layout

--- app/Modules/Users/Views/user/_form.blade.php
+++ app/Modules/Users/Views/user/_form.blade.php
@@ -1,38 +1,40 @@
+<?php $columnClass = $currentUser->isAdmin() ? 'col-md-4' : 'col-md-6'; ?>
+
 <div class="row">
     <div class="col-lg-8 col-md-12">
         <div class="row">
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('first_name')->label('First Name')->required() !!}
             </div>
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('last_name')->label('Last Name')->required() !!}
             </div>
+            @if($currentUser->isAdmin())
             <div class="col-md-4">
-                @if($currentUser->isAdmin())
                     {!! Former::select('groups[]')->label('Groups')->options($groups)->multiple()->dataPlaceholder('Select groups') !!}
-                @endif
             </div>
+            @endif
         </div>
 
         <div class="row">
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('username')->label('username')->required()->help('* Used for logging in. Must be unique.') !!}
             </div>
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('email')->label('Email')->help('* Must be unique.') !!}
             </div>
+            @if($currentUser->isAdmin())
             <div class="col-md-4">
-                @if($currentUser->isAdmin())
-                    {!! Former::select('type')->label('Type')->options($types)->required() !!}
-                @endif
+                {!! Former::select('type')->label('Type')->options($types)->required() !!}
             </div>
+            @endif
         </div>
 
         <div class="row">
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('phone')->label('Phone') !!}
             </div>
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 {!! Former::text('address')->label('Address') !!}
             </div>
         </div>
@@ -41,20 +43,20 @@
         {{--We will use javascript trigger to show them on page once required--}}
         @if($form == 'update')
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-12">
                     {!! Former::checkbox('change_password')->text('Change password? ') !!}
                 </div>
             </div>
         @endif
-        <div class="row {{ $form == 'update' ? 'change_password' : '' }}">
-            <div class="col-md-4">
+        <div class="row {{ $form == 'update' ? 'change_password hidden' : '' }}">
+            <div class="{{ $columnClass }}">
                 @if($form == 'update')
                     {!! Former::password('password')->label('Password') !!}
                 @else
                     {!! Former::password('password')->label('Password')->required() !!}
                 @endif
             </div>
-            <div class="col-md-4">
+            <div class="{{ $columnClass }}">
                 @if($form == 'update')
                     {!! Former::password('password_confirm')->label('Confirm Password') !!}
                 @else
