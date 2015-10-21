fa39aeeb21d9555b98c607bec4d79d116fa1610e
Nikola Pajic <nikola.pajic@gmail.com>
2015-10-21 11:21:40 +0200

    Change a way how icons on members list are displayed

--- app/Modules/Members/Views/member/index.blade.php
+++ app/Modules/Members/Views/member/index.blade.php
@@ -20,13 +20,13 @@
             <thead>
                 <tr>
                     <th width="50">#</th>
-                    <th width="25"></th>
-                    <th width="25"></th>
+                    <th width="10">P</th>
+                    <th width="10">A</th>
+                    <th width="40">M</th>
                     <th>Full Name</th>
                     <th class="hidden-md hidden-sm hidden-xs">Date of Birth</th>
                     <th class="hidden-md hidden-sm hidden-xs">Subscribed</th>
                     <th>Email</th>
-                    <th class="hidden-md hidden-sm hidden-xs">&nbsp;</th>
                     <th>Phone</th>
                     <th width="80">Actions</th>
                 </tr>
@@ -40,24 +40,24 @@
 
                                 @if($member->group && $member->active)
                                     @if($member->freeOfCharge)
-                                        <span class="fa fa-icon fa-money text-success" title="Free of charge member">&nbsp;</span>
+                                        <span class="btn btn-xs btn-circle btn-success" title="Free of charge member">&nbsp;</span>
                                     @elseif($member->group->data($today->year, $today->month, $member->id) && $member->group->data($today->year, $today->month, $member->id)->payed)
-                                        <span class="fa fa-icon fa-money text-success" title="Payed for this month">&nbsp;</span>
+                                        <span class="btn btn-xs btn-circle btn-success" title="Payed for this month">&nbsp;</span>
                                     @else
-                                        <span class="fa fa-icon fa-money text-danger" title="Not payed for this month">&nbsp;</span>
+                                        <span class="btn btn-xs btn-circle btn-danger" title="Not payed for this month">&nbsp;</span>
                                     @endif
                                 @endif
                             </td>
                             <td>
-                                <i class="fa fa-icon text-{{$member->active ? 'success fa-smile-o' : 'danger fa-frown-o'}}" title="{{$member->active ? 'Active' : 'Inactive'}} member"></i>
+                                <span class="btn btn-xs btn-circle btn-{{$member->active ? 'success' : 'danger'}}" title="{{$member->active ? 'Active' : 'Inactive'}} member">&nbsp;</span>
+                            </td>
+                            <td>
+                                <span class="btn btn-xs btn-circle {{ $member->getMedicalExaminationClass() }}" title="{{ $member->getMedicalExaminationTitle() }}">&nbsp;</span>
                             </td>
                             <td>{{ $member->full_name }}</td>
                             <td class="hidden-md hidden-sm hidden-xs">{{ $member->dob->format('d.m.Y') }}</td>
                             <td class="hidden-md hidden-sm hidden-xs">{{ $member->dos->format('d.m.Y') }} ({{ $member->dos->diffForHumans() }})</td>
                             <td>{{ $member->email }}</td>
-                            <td class="hidden-md hidden-sm hidden-xs">
-                                <span class="fa fa-icon fa-heart-o {{ $member->getMedicalExaminationClass() }}" title="{{ $member->getMedicalExaminationTitle() }}">&nbsp;</span>
-                            </td>
                             <td>{{ $member->phone }}</td>
                             <td>
                                 {!! Html::decode(link_to_route('member.show', '<i class="fa fa-pencil text-success"></i>', [$member->id], ['class' => 'btn btn-xs btn-default', 'title' => 'Update this item'])) !!}
