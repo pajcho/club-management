dcfbbdac7451e0adbc09cb6d2cc7c5175549bb98
Nikola Pajic <nikola.pajic@gmail.com>
2015-10-22 19:20:06 +0200

    Ditch pjax package in favour of middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,8 +17,7 @@ class Kernel extends HttpKernel
         'Illuminate\Session\Middleware\StartSession',
         'Illuminate\View\Middleware\ShareErrorsFromSession',
         'App\Http\Middleware\VerifyCsrfToken',
-
-        'JacobBennett\Pjax\PjaxMiddleware',
+        'App\Http\Middleware\FilterPjax',
     ];
 
     /**
